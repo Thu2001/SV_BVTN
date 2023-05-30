0e05487cba0013739d6726ac01305599da2bdae3
tinddt <tinddt@upsharework.com>
2023-05-30 19:38:47 +0700

    asdgagsd

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const QRCode = require('qrcode');
+var qr = require('qr-image');
 const port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+// app.get('/abc',async(req,res)=>{
+//     let img='';
+//     let qr= await QRCode.toDataURL('tui la tin dep trai!');
+//     console.log(qr);
+//     img = `<image src= " `+qr+ `" />`
+//     return res.send(img);
+// });
+
+app.get('/qr', function(req,res){
+    
+    console.log(qr.image('tins ddep trai', { type: 'png' }).pipe(res).originalUrl);
+
+ })
 
 let routes = require('./src/route/api') //importing route
 app.use("/api", routes)
